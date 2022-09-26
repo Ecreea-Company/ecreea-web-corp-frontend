07d3e81c9c64a7ecd2103f91fab36be1dab1f6a3
didojs <efvillantoyt@ucvvirtual.edu.pe>
2022-09-26 02:53:39 -0500

    Add: google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import 'styles/bases/main.scss'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import GoogleAnalytics from '@bradgarropy/next-google-analytics'
 
 function MyApp ({ Component, pageProps }: AppProps): JSX.Element {
   return (
@@ -8,6 +9,9 @@ function MyApp ({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
         <link rel="icon" type="image/x-icon" href="/ecreea.svg" />
       </Head>
+
+      <GoogleAnalytics measurementId="G-CQFPFTTPJ5" />
+
       <Component {...pageProps} />
     </>
   )
