076954f7f1f55c333e6b419142bdd0610d70f1ba
didojs <efvillantoyt@ucvvirtual.edu.pe>
2022-11-30 12:16:52 -0500

    Agregando cambios al blog

--- src/pages/blog/components/redes/Redes.component.tsx
+++ src/pages/blog/components/redes/Redes.component.tsx
@@ -9,20 +9,35 @@ import {
 function Redes ({ id }: any): JSX.Element {
   return (
     <div className={styles.Redes}>
-    <a href={`https://www.facebook.com/sharer/sharer.php?u=https://www.ecreea.com/blog/${id}`} >
-      <AiFillFacebook />
-    </a>
-    <a href='https://www.youtube.com/@Ecreea2Business'>
-      <AiFillYoutube />
-    </a>
-    <a href={`https://www.linkedin.com/sharing/share-offsite/?url=https://www.ecreea.com/blog/${id}`}>
-      <AiFillLinkedin />
-    </a>
-    <a href={`https://twitter.com/intent/tweet?text=Ecreea-Company&url=https://www.ecreea.com/blog/${id}&hashtags=Ecreea-Company`}>
-      <AiFillTwitterSquare />
-    </a>
-  </div>
-
+      <a
+        href={`https://www.facebook.com/sharer/sharer.php?u=https://www.ecreea.com/blog/${id}`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <AiFillFacebook />
+      </a>
+      <a
+        href="https://www.youtube.com/@Ecreea2Business"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <AiFillYoutube />
+      </a>
+      <a
+        href={`https://www.linkedin.com/sharing/share-offsite/?url=https://www.ecreea.com/blog/${id}`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <AiFillLinkedin />
+      </a>
+      <a
+        href={`https://twitter.com/intent/tweet?text=Ecreea-Company&url=https://www.ecreea.com/blog/${id}&hashtags=Ecreea-Company`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <AiFillTwitterSquare />
+      </a>
+    </div>
   )
 }
 
