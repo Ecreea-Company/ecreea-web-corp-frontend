853ec3abdee63107d85160f64659c52d1e47250f
didojs <efvillantoyt@ucvvirtual.edu.pe>
2022-12-02 11:06:14 -0500

    Agregando cambios al blog

--- next.config.js
+++ next.config.js
@@ -4,7 +4,14 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['res.cloudinary.com', 'xsgames.co', 'images.unsplash.com']
-  }
+  },
+  redirects: [
+    {
+      source: '/admin-strapi',
+      destination: 'https://strapi-production-d105.up.railway.app/admin',
+      statusCode: 301
+    }
+  ]
 }
 
 module.exports = nextConfig
