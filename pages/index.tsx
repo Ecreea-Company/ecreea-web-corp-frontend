5ee8e4409dc5c516d44485749afecd1c918c60ef
didojs <efvillantoyt@ucvvirtual.edu.pe>
2023-01-10 13:35:31 -0500

    fix: linkecreea extendible

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,14 @@
 import { Footer } from '@/contents'
 import { Public } from '@/layouts'
-import { CreacionDeImpacto, CrearParaCrear, Ecreadores, InspirarCrear, NuestraEsencia, Transformamos, UltimasInformaciones } from '@/pages/home/content'
+import {
+  CreacionDeImpacto,
+  CrearParaCrear,
+  Ecreadores,
+  InspirarCrear,
+  NuestraEsencia,
+  Transformamos,
+  UltimasInformaciones
+} from '@/pages/home/content'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
@@ -11,9 +19,9 @@ const Home: NextPage = () => {
       <Transformamos />
       <CreacionDeImpacto />
       <Ecreadores />
-      <NuestraEsencia/>
+      <NuestraEsencia />
       <UltimasInformaciones />
-      <Footer/>
+      <Footer />
     </Public>
   )
 }
--- pages/inversores.tsx
+++ pages/inversores.tsx
@@ -14,6 +14,7 @@ const Creadores: NextPage = () => {
       <Costo />
       <Huella />
       <Footer />
+
     </Public>
   )
 }
