bd108e2f7f96729ebe671641542af4155d447e25
didojs <efvillantoyt@ucvvirtual.edu.pe>
2023-01-12 14:08:57 -0500

    fix: loading

--- src/components/loading-pages/LoadingPages.component.tsx
+++ src/components/loading-pages/LoadingPages.component.tsx
@@ -1,15 +1,19 @@
 import styles from './LoadingPages.module.scss'
 import Lottie from 'react-lottie-player'
 import animateLoading from '@/assets/lotties/loading-lineal.json'
+import Logo from '@/assets/logo/Logo.component'
 
 const LoadingPages = (): JSX.Element => {
   return (
     <div className={styles.Loading}>
+      <div className={styles.Loading__logo}>
+        <Logo />
+      </div>
       <Lottie
         loop
         animationData={animateLoading}
         play
-        style={{ width: 500 }}
+        className={styles.Loading__lottie}
         rendererSettings={{ preserveAspectRatio: 'xMidYMid slice' }}
       />
     </div>
