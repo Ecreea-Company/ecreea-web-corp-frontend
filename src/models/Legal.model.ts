4b9aa5929047988a4437ba7ce04c3ad37c13f547
Diego <dicornyt@gmail.com>
2023-01-22 18:35:45 -0500

    feat: model legal

---
+++ src/models/Legal.model.ts
@@ -0,0 +1,4 @@
+export interface ItemLegalProps {
+  title?: string
+  data: string
+}
---
+++ src/pages/legal/components/index.ts
@@ -0,0 +1,2 @@
+export { default as Dropdown } from './dropdown/Dropdown.component'
+export { default as ListLegal } from './list-legal/ListLegal.component'
