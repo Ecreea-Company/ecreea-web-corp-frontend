1065ddfdcd8310e591b2368f2edc5639217f2c17
Diego <dicornyt@gmail.com>
2023-01-23 04:23:24 -0500

    refactor: legal index

--- pages/legal.tsx
+++ pages/legal.tsx
@@ -1,20 +1,31 @@
 import { Public } from '@/layouts'
 import type { NextPage } from 'next'
 import styles from '@styles/legal/Legal.module.scss'
-import { Dropdown, ListLegal } from '@/pages/legal/components'
+import { Dropdowns } from '@/components'
 import { DataLegal } from '@/data'
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+import remarkGfm from 'remark-gfm'
 
 const Legal: NextPage = () => {
-  const firstData = DataLegal[0]
-  const secondData = DataLegal.slice(1)
+  const TermYCond = DataLegal[0]
+  const ContentDrop = DataLegal.slice(1)
 
   return (
     <Public titlePage="Legal">
       <div className={styles.Section}>
         <h1 className={styles.Section__title}>Legal Ecreea</h1>
-        <ListLegal data={firstData.data}/>
+        <div className={styles.Section__paragragh}>
+          <ReactMarkdown remarkPlugins={[remarkGfm]}>
+            {TermYCond.data}
+          </ReactMarkdown>
+        </div>
       </div>
-      <Dropdown itemList={secondData}/>
+
+      <div>
+        {ContentDrop.map((item, index) => (
+          <Dropdowns key={index} title={item.title} content={item.data} />
+        ))}
+      </div>
     </Public>
   )
 }
