77727d9393023062c34923339bef0aa90047d52c
Diego <dicornyt@gmail.com>
2023-01-27 11:02:56 -0500

    refactor: quitar cambios de page 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,36 +5,6 @@ import Logo from '@/assets/logo/Logo.component'
 import { MdOutlineKeyboardArrowRight } from 'react-icons/md'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
-import { DataTabla } from '@/data'
-
-const Table = () => {
-  console.log(DataTabla)
-  return (
-    <div className={styles.Table}>
-      <ul className={styles.Table__titles}>
-        {
-          DataTabla.titles.map((title, index) => {
-            return (
-              <li key={index}>{title}</li>
-            )
-          })
-        }
-      </ul>
-
-      {
-          DataTabla.content.map((item, index) => {
-            return (
-              <ul key={index} className={styles.Table__rows}>
-              <li >{item.colummn1}</li>
-              <li >{item.colummn2}</li>
-              </ul>
-            )
-          })
-        }
-
-    </div>
-  )
-}
 
 export default function FourOhFour () {
   const [nameUrl, setNameUrl] = useState<string>('')
@@ -46,7 +16,7 @@ export default function FourOhFour () {
 
   return (
     <section className={styles.Page}>
-      {/* <div className={styles.Page__gato}>
+      <div className={styles.Page__gato}>
         <Gato404 />
       </div>
       <Logo />
@@ -62,10 +32,7 @@ export default function FourOhFour () {
             <MdOutlineKeyboardArrowRight />
           </span>
         </h5>
-      </LinkEcreea> */}
-
-      <Table/>
-
+      </LinkEcreea>
     </section>
   )
 }
