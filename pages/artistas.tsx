4d7b2cae0350d79268bbadcdf6b6f5e9a84425a0
didojs <efvillantoyt@ucvvirtual.edu.pe>
2023-01-30 23:45:39 -0500

    refactor: cambiando estrutura de page grid

--- pages/artistas.tsx
+++ pages/artistas.tsx
@@ -9,58 +9,57 @@ const Artistas: NextPage = () => {
 
   return (
     <Public titlePage="Artistas">
-      <section className={styles.Hero}>
-        <Image
-          layout="fill"
-          src="/pages/artistas/art_1.webp"
-          objectFit={width > 1200 ? 'fill' : 'cover'}
-          priority
-        />
-        <div className={styles.Hero__parrafos}>
-          <h1 className={styles.Hero__parrafo}>
-            <span>Artistas &</span> Creadores
-          </h1>
-          <h1 className={styles.Hero__parrafo__left}>
-            <p>{'«El arte está ligado a lo que todavía no se crea.»'}</p>
-            <span>Eduardo Chillida</span>
-          </h1>
-        </div>
-      </section>
-      <section className={styles.Cita}>
-        <p>
-          “Somos artistas que crean valor en la vida de nuestros usuarios,
-          trabajamos en mas que una empresa, trabajamos en equipo, trabajamos en
-          arte, trabajamos para ecreear un mundo mejor.”
-        </p>
-        <span>Holiver U.</span>
-      </section>
-      <Image
-        layout="responsive"
-        width={1920}
-        height={1080}
-        src="/pages/artistas/art_2.webp"
-      />
-      <section className={styles.Cita}>
-        <Image
-          layout="fill"
-          src="/pages/artistas/bg_carlos.jpg"
-          objectFit='cover'
-          // objectPosition='10% 55%'
-          objectPosition={width > 1200 ? '10% 80%' : '10% 55%'}
-          priority
-        />
-        <p>
-          “Vivimos en una revolución con los sueños y caminos de cada ecreador
-          utópico. El primer paso a construir lo que nadie imaginó ni creó.”
-        </p>
-        <span>Carlos A.</span>
-      </section>
-      <Image
-        layout="responsive"
-        width={1920}
-        height={1279}
-        src="/pages/artistas/art_3.webp"
-      />
+      <div className={styles.Page}>
+        <section className={styles.Hero}>
+          <Image
+            layout="fill"
+            src="/pages/artistas/art_1.png"
+            objectFit={width > 1200 ? 'fill' : 'cover'}
+            priority
+          />
+          <div className={styles.Hero__parrafos}>
+            <h1 className={styles.Hero__parrafo}>
+              <span>Artistas &</span> Creadores
+            </h1>
+            <h1 className={styles.Hero__parrafo__left}>
+              <p>{'«El arte está ligado a lo que todavía no se crea.»'}</p>
+              <span>Eduardo Chillida</span>
+            </h1>
+          </div>
+        </section>
+        <section className={styles.Holiver}>
+          <p>
+            “Somos artistas que crean valor en la vida de nuestros usuarios,
+            trabajamos en mas que una empresa, trabajamos en equipo, trabajamos
+            en arte, trabajamos para ecreear un mundo mejor.”
+          </p>
+          <span>Holiver U.</span>
+        </section>
+        <section className={styles.Letrero}>
+          <Image
+            layout="responsive"
+            width={1920}
+            height={1080}
+            src="/pages/artistas/art_2.png"
+          />
+        </section>
+        <section className={styles.Carlos}>
+          <p>
+            “Vivimos en una revolución con los sueños y caminos de cada ecreador
+            utópico. El primer paso a construir lo que nadie imaginó ni creó.”
+          </p>
+          <span>Carlos A.</span>
+        </section>
+
+        <section className={styles.Foto}>
+          <Image
+            layout="responsive"
+            width={1920}
+            height={1279}
+            src="/pages/artistas/art_3.png"
+          />
+        </section>
+      </div>
     </Public>
   )
 }
