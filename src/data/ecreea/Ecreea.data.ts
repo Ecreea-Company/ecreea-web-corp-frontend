2c4704c6d66689e96a13473c073d963c0c833099
Diego <dicornyt@gmail.com>
2023-02-06 11:18:34 -0500

    feat: page ecreea data

---
+++ src/data/ecreea/Ecreea.data.ts
@@ -0,0 +1,22 @@
+import { ItemLinkProps } from '@/models'
+
+export const DropItems: ItemLinkProps[] = [
+  {
+    name: 'El Trabajo en Ecreea',
+    url: '/ecreea/el-trabajo-en-ecreea',
+    desactivar: false,
+    id: 1
+  },
+  {
+    name: 'Aprende con nosotros',
+    url: '/ecreea/aprende-con-nosotros',
+    desactivar: false,
+    id: 2
+  },
+  {
+    name: 'Buscar oportunidades',
+    url: '/contacto',
+    desactivar: true,
+    id: 3
+  }
+]
