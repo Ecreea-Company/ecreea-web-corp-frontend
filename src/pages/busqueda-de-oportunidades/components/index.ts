a4de3aad412360754763a034ccf31b10614cc34e
DiegoCP27 <dicornyt@gmail.com>
2023-02-28 10:35:44 -0500

    feat: index component

---
+++ src/pages/busqueda-de-oportunidades/components/index.ts
@@ -0,0 +1,7 @@
+export { default as DropdownOp } from './dropdown/Dropdown.component'
+export { default as LinkRedirectOp } from './link/LinkRedirect.component'
+export { default as TexfieldOp } from './textfield/Textfield.component'
+export { default as ButtonOp } from './button/Button.component'
+export { default as TypographyOp } from './typography/Typography.component'
+export { default as TrabajoOP } from './trabajo/Trabajo.component'
+export { default as SlideButtonOP } from './filter-button/FilterButton.component'
