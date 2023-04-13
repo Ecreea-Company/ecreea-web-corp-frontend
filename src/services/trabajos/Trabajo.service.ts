9ca07b8c8d01ed2a872db059a5ab478d5bb2ad17
DiegoCP27 <dicornyt@gmail.com>
2023-04-12 20:36:20 -0500

    refactor: trabajo service

--- src/services/trabajos/Trabajo.service.ts
+++ src/services/trabajos/Trabajo.service.ts
@@ -1,15 +1,15 @@
 export const getTrabajos = async () => {
   const res = await fetch(
-    'http://localhost:1337/api/jobs?populate=*'
+    'http://127.0.0.1:1337/api/jobs?populate=*'
   )
 
   return await res.json()
 }
 
-export const getTrabajosByPage = async (page: string | number | string[]) => {
+export const getTrabajosBySlug = async (url: string | string[] | undefined) => {
   const res = await fetch(
-    `http://localhost:1337/api/posts?populate=*&sort[0]=id%3Adesc&pagination[page]=${page}&pagination[pageSize]=6`
-  )
+    `http://127.0.0.1:1337/api/jobs?filters[slug][$eq]=${url}&populate=*`
+  ).then(async (res) => await res.json())
 
-  return await res.json()
+  return res
 }
