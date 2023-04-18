4b172b74cb4a940678110749ee49b2babc60f215
DiegoCP27 <dicornyt@gmail.com>
2023-04-18 13:01:31 -0500

    feat: getTrabajosByPage service

--- src/services/trabajos/Trabajo.service.ts
+++ src/services/trabajos/Trabajo.service.ts
@@ -1,3 +1,11 @@
+export const getTrabajosByPage = async (page: string | number | string[]) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/jobs?populate=*&sort[0]=id%3Adesc&pagination[page]=${page}&pagination[pageSize]=5`
+  )
+
+  return await res.json()
+}
+
 export const getTrabajos = async () => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/jobs?populate=*`
