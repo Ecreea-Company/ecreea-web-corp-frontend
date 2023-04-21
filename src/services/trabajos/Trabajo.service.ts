b5938767d3eede76bc1974df4b75cf198a870621
DiegoCP27 <dicornyt@gmail.com>
2023-04-21 01:46:48 -0500

    refactor: trabajo service

--- src/services/trabajos/Trabajo.service.ts
+++ src/services/trabajos/Trabajo.service.ts
@@ -1,6 +1,18 @@
-export const getTrabajosByPage = async (page: string | number | string[]) => {
+const filtersToQueryString = (filters: Record<string, any>) => {
+  return Object.entries(filters)
+    .map(([key, value]) => {
+      if (Array.isArray(value)) {
+        return value.map((v) => `filters[${key}][slug][$eq]=${v}`).join('&')
+      }
+      return `filters[${key}][slug][$eq]=${value}`
+    })
+    .join('&')
+}
+
+export const getTrabajosByPage = async (page: string | number | string[], filters: Record<string, any> = {}) => {
+  const filterQuery = filtersToQueryString(filters)
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/jobs?populate=*&sort[0]=id%3Adesc&pagination[page]=${page}&pagination[pageSize]=5`
+    `${process.env.NEXT_PUBLIC_API_URL}/jobs?populate=*&sort[0]=id%3Adesc&pagination[page]=${page}&pagination[pageSize]=5&${filterQuery}`
   )
 
   return await res.json()
