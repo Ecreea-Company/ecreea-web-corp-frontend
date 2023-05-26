629db4be6be1ac9aaf8f318df7ce9e8e62d2dcac
Diego Carbajal <dicornyt@gmail.com>
2023-05-26 02:10:26 -0500

    refact: eliminar width y añadir los nombres

--- src/pages/busqueda-de-oportunidades/components/list-of-dropdown/ListOfDropdown.component.tsx
+++ src/pages/busqueda-de-oportunidades/components/list-of-dropdown/ListOfDropdown.component.tsx
@@ -1,23 +1,24 @@
 import { Dropdown } from '@/pages/busqueda-de-oportunidades/components'
 import { useRouter } from 'next/router'
 
-const ListOfDropdown = ({
-  width,
-  filters
-}: {
-  width?: string
-  filters: any
-}) => {
+const ListOfDropdown = ({ filters }: any) => {
   const router = useRouter()
+
+  const filterKeyMap: any = {
+    ubicacions: 'Ubicación',
+    'tipo-contratoes': 'Tipo de Contrato',
+    'area-trabajos': 'Área de Trabajo',
+    'modalidad-trabajos': 'Modalidad',
+    companias: 'Empresa'
+  }
 
   return (
     <div>
       {filters.map((filter: any) => (
         <Dropdown
           key={filter.name}
-          name={filter.name}
+          name={filterKeyMap[filter.name]}
           options={filter.options}
-          width={width}
           router={router}
         />
       ))}
